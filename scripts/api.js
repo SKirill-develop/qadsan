68dc4d53d48a7db65241ec2213eef38caf1dcd56
Kirill Shipilov <shipilov20@yandex.ru>
2021-11-26 17:48:44 +0400

    css update

--- scripts/api.js
+++ scripts/api.js
@@ -1,9 +1,1 @@
-var url = 'https://centus.loan/api.php?x=hello';
 
-fetch(url)
-.then(function (response) {
-  return response.text();
-})
-.then(function (body) {
-  console.log(body);
-});
--- scripts/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-if($_SERVER["REQUEST_METHOD"]=="POST"){
-  $input = file_get_contents('php://input');
-  $input = json_decode($input);
-  echo '23123';
-}
-?>
